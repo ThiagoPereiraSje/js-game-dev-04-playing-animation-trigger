d836a0b5e109d3433dede55b1f9d8c70289263d8
Thiago Pereira <thiagopereiramail@gmail.com>
2022-12-03 08:18:56 -0300

    Add canvas rotation effect

--- src/classes/Explosion.ts
+++ src/classes/Explosion.ts
@@ -5,6 +5,7 @@ export class Explosion {
   y: number;
   frame = 0;
   timer = 0;
+  angle = Math.random() * 6.2; // Angle in radians
   spriteWidth = 200;
   spriteHeight = 179;
   width = this.spriteWidth * 0.7;
@@ -12,9 +13,13 @@ export class Explosion {
   image = new Image();
 
   constructor(x: number, y: number) {
-    this.x = x - this.width / 2;
-    this.y = y - this.height / 2;
+    // this.x = x - this.width / 2; // Centerilize animation horizontally
+    // this.y = y - this.height / 2; // Centerilize animation vertically
     this.image.src = boom;
+
+    // When rotate, center image in draw method
+    this.x = x;
+    this.y = y;
   }
 
   update() {
@@ -26,16 +31,24 @@ export class Explosion {
   }
 
   draw(ctx: CanvasRenderingContext2D) {
+    // Image rotation
+    ctx.save(); // Save the current state of the canvas to ensure rotation only affects this rendering
+    ctx.translate(this.x, this.y); // Ratation central point
+    ctx.rotate(this.angle);
+
     ctx.drawImage(
       this.image,
       this.spriteWidth * this.frame,
       0,
       this.spriteWidth,
       this.spriteHeight,
-      this.x,
-      this.y,
+      0 - this.width / 2, // The x and y coordinates are captured in the translate method,
+      0 - this.height / 2, // so you only need to center the image
       this.width,
       this.height
     );
+
+    // Restore the canvas context to the original save point
+    ctx.restore();
   }
 }
